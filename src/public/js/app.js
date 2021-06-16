7b838e4fdd560be39c8ddfdf35e49d99c04e81a1
Mako0722 <zhongtiancheng79@gmail.com>
2021-06-16 15:04:42 +0900

    タグの自動補完　 ハッシュタグ風 タグごとの記事一覧画面

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2074,26 +2074,26 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({
   components: {
     VueTagsInput: _johmun_vue_tags_input__WEBPACK_IMPORTED_MODULE_0___default.a
   },
+  props: {
+    initialTags: {
+      type: Array,
+      "default": []
+    },
+    autocompleteItems: {
+      type: Array,
+      "default": []
+    }
+  },
   data: function data() {
     return {
       tag: '',
-      tags: [],
-      autocompleteItems: [{
-        text: 'Spain'
-      }, {
-        text: 'France'
-      }, {
-        text: 'USA'
-      }, {
-        text: 'Germany'
-      }, {
-        text: 'China'
-      }]
+      tags: this.initialTags
     };
   },
   computed: {
@@ -2143,7 +2143,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "\n.vue-tags-input .ti-tag {\n  background: transparent;\n  border: 1px solid #747373;\n  color: #747373;\n  margin-right: 4px;\n  border-radius: 0px;\n  font-size: 13px;\n}\n", ""]);
+exports.push([module.i, "\n.vue-tags-input .ti-tag {\n  background: transparent;\n  border: 1px solid #747373;\n  color: #747373;\n  margin-right: 4px;\n  border-radius: 0px;\n  font-size: 13px;\n}\n.vue-tags-input .ti-tag::before {\n  content: \"#\";\n}\n", ""]);
 
 // exports
 
@@ -21313,7 +21313,8 @@ var render = function() {
         attrs: {
           tags: _vm.tags,
           placeholder: "タグを5個まで入力できます",
-          "autocomplete-items": _vm.filteredItems
+          "autocomplete-items": _vm.filteredItems,
+          "add-on-key": "[13, 32]"
         },
         on: {
           "tags-changed": function(newTags) {
